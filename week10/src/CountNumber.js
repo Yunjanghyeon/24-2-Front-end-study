9a2f63a240650bc8bade63332fb715f889049093
Yunjanghyeon <jaden2323@naver.com>
2024-11-15 16:01:22 +0900

    week10 링크만들야지

---
+++ week10/src/CountNumber.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+function CountNumber(properties) {
+    function coloredNumber() {
+        if (properties.count > 0){
+            return `rgb(${properties.count*10}, 0, 0)`
+        }
+        if (properties.count < 0){
+            return `rgb(0,${properties.count*-10},0)`
+        }
+        if (properties.count === 0){
+            return `rgb(0,0,0)`
+        }
+    }
+    
+    const h2Style = {
+
+        fontSize: '30px',
+       
+        color:`${coloredNumber()}`
+ }
+
+
+ return (
+
+ <h2 style={h2Style}>{properties.count}</h2>
+
+ )
+
+}
+
+
+export default CountNumber
